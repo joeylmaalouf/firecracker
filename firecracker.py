697f4225d7419f9fb06683640a28c01631593919
joeylmaalouf <joeylmaalouf@gmail.com>
2015-03-25 09:25:26 -0400

    tweaks

--- firecracker.py
+++ firecracker.py
@@ -9,6 +9,9 @@ import gtk
 def main(argv):
 	if len(argv) < 2:
 		argv.append("./example.cfg")
+		print("\nThis demo uses the provided example configuration.")
+		print("You can use your own by typing")
+		print("    python2 "+argv[0].split("/")[-1]+" <config file>\n")
 
 	config_list = parse(argv[1])
 	for item in config_list:
@@ -20,4 +23,4 @@ if __name__ == "__main__":
 	main(sys.argv)
 
 # to do: close windows individually instead of all at once with escape button
-# make sure to do gtk.main_quit() so everything actually closes though
+# make sure to do gtk.main_quit() after the last one so everything actually closes though
