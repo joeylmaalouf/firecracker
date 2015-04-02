c3ebcb1483fbc657774087ad7fbee8ee338ea665
joeylmaalouf <joeylmaalouf@gmail.com>
2015-04-02 14:33:38 -0400

    began config generator

--- firecracker_config_generator.py
+++ firecracker_config_generator.py
@@ -1,8 +1,25 @@
 import sys
 
+import pygtk
+pygtk.require("2.0")
+import gtk
 
-def main():
-	pass
+
+class PyWindow(gtk.Window):
+	def __init__(self):
+		super(PyWindow, self).__init__()
+		self.connect("destroy", gtk.main_quit)
+		self.set_size_request(400, 400)
+		self.set_position(gtk.WIN_POS_CENTER)
+		self.show()
+
+	def main(self):
+		gtk.main()
+
+
+def main(argv):
+	win = PyWindow()
+	win.main()
 
 
 if __name__ == "__main__":
