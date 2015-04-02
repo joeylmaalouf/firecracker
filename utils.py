c2c9b5c88b16bbdce9464ad6a200f3a893d3eee4
Tom <tah20112@gmail.com>
2015-04-02 13:57:40 -0400

    Enumerated parse

--- utils.py
+++ utils.py
@@ -1,11 +1,9 @@
-import cairo
-from datetime import datetime
-from json import loads
-from pattern.web import URL
-
 import pygtk
 pygtk.require("2.0")
 import gtk
+
+import cairo
+from datetime import datetime
 
 
 class FCManager(object):
@@ -29,17 +27,22 @@ class FCWindow(object):
 		self.watcher = None
 
 		self.label = gtk.Label(item.text)
-		self.label.set_angle(item.angle)
 		self.label.set_markup("<span face='"+item.font+"' size='"+str(item.text_size*1000)+"'>"+str(item.text)+"</span>")
 		self.label.set_justify(gtk.JUSTIFY_CENTER)
 		self.label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse(item.text_color))
+
+		# file_name = "images/logoWeb.png"
+		# pixbuf = gtk.gdk.pixbuf_new_from_file(file_name)
+		# pixmap, mask = pixbuf.render_pixmap_and_mask()
+		# self.image = gtk.Image()
+		# self.image.set_from_pixmap(pixmap, mask)
 
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.resize(item.w, item.h)
 		self.window.move(item.x, item.y)
 		self.window.set_title(item.title)
 		self.window.set_opacity(item.alpha)
-		self.window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DOCK)  # uncomment this to remove the taskbar icon, comment to add it back
+		self.window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DOCK)  # uncomment this to remove the taskbar icon
 		self.window.set_keep_below(True)
 		self.window.set_icon_from_file("images/logo.png")
 		self.window.stick()
@@ -53,32 +56,26 @@ class FCWindow(object):
 
 		self.box = gtk.EventBox()
 		self.box.set_visible_window(False)
-		self.box.connect("button_press_event", self.onclick)
-		self.box.connect("button_release_event", self.onrelease)
-		self.box.connect("motion_notify_event", self.mousemove)
-
+		self.box.connect('button_press_event',self.onclick)
+		self.box.connect('button_release_event', self.onrelease)
+		self.box.connect('motion_notify_event', self.mousemove)
+		
+		# self.window.add(self.image)
 		self.window.add(self.box)
 		self.box.add(self.label)
 		self.window.set_decorated(False)
 		self.window.show_all()
 
-
 	def update(self):
-		if self.vals.type == "CLOCK":
+		if self.vals.clock:
 			time = datetime.now().time()
 			time_string = "{0:02d}:{1:02d}:{2:02d}".format(time.hour, time.minute, time.second)
 			self.label.set_markup("<span size='"+str(self.vals.text_size*1000)+"'>"+time_string+"</span>")
-		elif self.vals.type == "WEATHER":
-			data = loads(URL("http://api.openweathermap.org/data/2.5/weather?zip="+self.vals.zip_code+",us").download())
-			status = data["weather"][0]["main"].lower()
-			temp = (float(data["main"]["temp"])-273.15)*9/5+32
-			weather_string =  "It is {0} outside, and\n{1:0.2f} degrees Fahrenheit.".format(status, temp)
-			self.label.set_markup("<span size='"+str(self.vals.text_size*1000)+"'>"+weather_string+"</span>")
-
 		return True
 
 	def key_press(self, widget, event):
-		x, y = self.window.get_position()
+		# Get current position of window
+		x,y = self.window.get_position()
 
 		if gtk.gdk.keyval_name(event.keyval) == "Escape":
 			self.write_config(self.vals)
@@ -86,12 +83,15 @@ class FCWindow(object):
 			self.watcher.num_windows -= 1
 			if self.watcher.num_windows == 0:
 				gtk.main_quit()
-
-		elif gtk.gdk.keyval_name(event.keyval) == "Up":    y -= 5
-		elif gtk.gdk.keyval_name(event.keyval) == "Down":  y += 5
-		elif gtk.gdk.keyval_name(event.keyval) == "Left":  x -= 5
-		elif gtk.gdk.keyval_name(event.keyval) == "Right": x += 5
-		self.window.move(x, y)
+		elif gtk.gdk.keyval_name(event.keyval) == "Up":
+			y = y - 5
+		elif gtk.gdk.keyval_name(event.keyval) == "Down":
+			y = y + 5
+		elif gtk.gdk.keyval_name(event.keyval) == "Left":
+			x = x - 5
+		elif gtk.gdk.keyval_name(event.keyval) == "Right":
+			x = x + 5
+		self.window.move(x,y)
 
 	def onclick (self, widget, event):
 		self.window.drag = True
@@ -102,8 +102,9 @@ class FCWindow(object):
 		self.window.drag = False
 
 	def mousemove(self, widget, event):
-		x, y = self.window.get_position()
-		self.window.move(x+int(event.x-self.drag_x), y+int(event.y-self.drag_y))
+		x,y = self.window.get_position()
+		self.window.move(x+int(event.x-self.drag_x),y+int(event.y-self.drag_y))
+		#self.x, self.y = self.layout.child_get(self.event_box,'x','y')
 
 	def transparent_expose(self, widget, event):
 		cr = widget.window.cairo_create()
@@ -123,12 +124,11 @@ class FCItem(object):
 		the custom data format used for storing data
 		read in from configuration files.
 	"""
-	def __init__(self, type_str):
-		self.title = "Firecracker"
-		self.type = type_str
+	def __init__(self, id_str):
+		self.title = id_str
 		self.x = 0
 		self.y = 0
-		# IMPORTANT: if we make the default size (1, 1), then there are no errors and we can move the window anywhere
+		# IMPORTANT: if we make the default size below (1,1), then there are no errors and we can move the window anywhere
 		self.w = 1
 		self.h = 1
 		self.alpha = 1.0
@@ -136,14 +136,12 @@ class FCItem(object):
 		self.text_color = "#FFFFFF"
 		self.text_size = 16
 		self.font = "Helvetica"
-		self.angle = 0
 		self.update_timer = 1000
-		self.zip_code = "00000"
+		self.clock = False
 
 
 	def __str__(self):
 		return ("Title: "+self.title+"\n"+
-		"Type: "+str(self.type)+"\n"+
 		"Position: "+str(self.x)+", "+str(self.y)+"\n"+
 		"Size: "+str(self.w)+", "+str(self.h)+"\n"+
 		"Transparency: "+str(self.alpha)+"\n"+
@@ -151,9 +149,8 @@ class FCItem(object):
 		"Text Color: "+self.text_color+"\n"+
 		"Text Size: "+str(self.text_size)+"\n"+
 		"Font: "+self.font+"\n"+
-		"Zip: "+str(self.zip_code)+"\n"+
+		"Clock: "+str(self.clock)+"\n"+
 		"Update timer: "+str(self.update_timer))
-
 
 
 def parse(filepath):
@@ -172,15 +169,14 @@ def parse(filepath):
 		
 		elif line[0] == "<":
 			in_item = True
-			item = FCItem(line[1:].strip().upper())
+			item = FCItem('FireCracker') # FCItem(line[1:].strip())
 		
 		elif line[0] == ">":
 			datalist.append(item)
 			in_item = False
 		
 		elif "=" in line and in_item:
-			(key, val) = [i.strip() for i in line.split("=", 1)]
-			key = key.lower()
+			key, val = [i.strip() for i in line.split("=", 1)]
 			if key == "text":
 				item.text = val
 			elif key == "pos_x":
@@ -199,10 +195,8 @@ def parse(filepath):
 				item.text_color = "#"+val
 			elif key == "font":
 				item.font = val
-			elif key == "angle":
-				item.angle = int(val)
-			elif key == "zip_code":
-				item.zip_code = str(val)
+			elif key == "clock" and val.lower() == "true":
+				item.clock = True
 			elif key == "update":
 				item.update_timer = int(val)
 
