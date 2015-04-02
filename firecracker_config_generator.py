c6aa4d70aec5f131aaf223c3dcff1929f8eb7332
joeylmaalouf <joeylmaalouf@gmail.com>
2015-04-02 14:57:59 -0400

    doc

--- firecracker_config_generator.py
+++ firecracker_config_generator.py
@@ -34,6 +34,11 @@ class MainWindow(gtk.Window):
 
 	def on_press(self, widget, data):
 		print(self.counter.get_value_as_int())
+		# define a new itemwindow class, and spawn n many of them? somehow combine all of those results?
+		# OR: just have this one window, and change the button to append the current properties to a config file
+		# (add file path as a field to fill), and clear the fields each time they press the button
+		# so they can fill in new values for the next item (make sure to display a label at the top telling them
+		# that the item has been successfully appended and that now they're doing a new item)
 
 
 def main(argv):
