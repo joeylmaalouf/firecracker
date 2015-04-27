ac71b8362c3443135d8c9f7b389e73c1969f8e0f
joeylmaalouf <joeylmaalouf@gmail.com>
2015-04-27 15:03:32 -0400

    undo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cwd=`pwd`
-echo "start on runlevel [5]
-stop on runlevel [!5]
+echo "start on runlevel [2345]
+stop on runlevel [!2345]
 exec $cwd/firecracker.py" > /etc/init/firecracker_startup.conf
