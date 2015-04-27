6ea47a00b6b8b91248cb25cbacd833ec092955d0
joeylmaalouf <joeylmaalouf@gmail.com>
2015-04-27 13:37:01 -0400

    CLOUDS

--- firecracker_utils.py
+++ firecracker_utils.py
@@ -84,7 +84,7 @@ class FCWindow(object):
 		elif self.vals.type == "WEATHER":
 			try:
 				data = loads(URL("http://api.openweathermap.org/data/2.5/weather?zip="+self.vals.zip_code+",us").download())
-				status = data["weather"][0]["main"].lower()
+				status = data["weather"][0]["description"].lower()
 				# status = {"clouds":"It's cloudy outside.", "clear":"It's clear outside.", "rain":"It's raining outside."}[status]
 				temp = (float(data["main"]["temp"])-273.15)*9/5+32
 				weather_string =  "Weather: {0}\nTemperature: {1:0.2f} degrees Fahrenheit.".format(status, temp)
